3abc14dee982e23fa42566aa88682d8e2a638cf3
Harry <hcfairbanks@gmail.com>
2017-12-19 22:24:07 -0400

    Worked on cat model rspec specs

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,5 @@
 class Cat < ApplicationRecord
   mount_base64_uploader :picture, PictureUploader
+  validates :name, presence: true
   #serialize :picture, JSON # If you use SQLite, add this line.
 end
--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -12,21 +12,67 @@ RSpec.describe Cat, type: :model do
     }
   }
 
-  # let(:invalid_attributes) {
-  #   {
-  #     name: "",
-  #     picture: ""
-  #   }
-  # }
+  let(:update_attributes) {
+    {
+      name: "John",
+      picture: data_url
+    }
+  }
 
-  it "create a cat with valid attributes" do
+  let(:invalid_attributes) {
+    {
+      name: "",
+      picture: ""
+    }
+  }
+
+  it "is valid with a name and an image" do
     test_cat = Cat.create(valid_attributes)
     expect(test_cat).to be_valid
   end
 
-  it "can't create a cat with invalid attributes" do
-    #expect(Cat.create(invalid_attributes)).to be_falsey
+  it "is invalid without a name" do
+    expect(Cat.create(invalid_attributes)).to be_invalid
   end
 
+  it "returns the correct error msg" do
+    cat = Cat.create(invalid_attributes)
+    expect(cat.errors[:name]).to include("can't be blank")
+  end
+
+  it "can update with valid attributes" do
+    cat = Cat.create(valid_attributes)
+    cat.update(update_attributes)
+    cat.reload
+    expect(cat).to be_valid
+  end
+
+  # it "can't update with invalid attributes" do
+  #   test_cat = Cat.create(valid_attributes)
+  #   test_cat.update(invalid_attributes)
+  #   test_cat.reload
+  #   expect(test_cat).to be_invalid
+  # end
+
+  describe "delete a cat" do
+
+    it "decreases the cat count by 1" do
+      cat = Cat.create(valid_attributes)
+      expect {Cat.destroy(cat.id)}.to change(Cat, :count).by(-1)
+    end
+
+    it "can't be found in db" do
+      cat = Cat.create(valid_attributes)
+      Cat.destroy(cat.id)
+      expect(Cat.where(id: cat.id).count).to eq(0)
+    end
+
+    # it "removes the cat image" do
+    #   test_user = Cat.create(valid_attributes)
+    #   Cat.destroy(test_user.id)
+    #   expect(Image.where(id: test_user.image.id).count).to eq(0)
+    # end
+
+  end
 
 end
