416731e71af35d50579d586daa5eb70cdcd9771b
Harry <hcfairbanks@gmail.com>
2017-12-20 23:05:34 -0400

    removes directory when cat model is destroyed

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,5 +1,19 @@
 class Cat < ApplicationRecord
+
   mount_base64_uploader :picture, PictureUploader
   validates :name, presence: true
-  #serialize :picture, JSON # If you use SQLite, add this line.
+  after_destroy :remove_picture_directory
+
+  protected
+
+  def remove_picture_directory
+    FileUtils.remove_dir(File.join(Rails.root,
+                                  "public",
+                                  "uploads",
+                                  Rails.env,
+                                  "cat",
+                                  "picture",
+                                  self.id.to_s), force: true)
+  end
+
 end
