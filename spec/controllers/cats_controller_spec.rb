b6381b8259817eab9329578dea066a80b4b18ae0
Harry <hcfairbanks@gmail.com>
2017-12-28 21:38:59 -0500

    Working on custom update messages

--- spec/controllers/cats_controller_spec.rb
+++ spec/controllers/cats_controller_spec.rb
@@ -27,6 +27,20 @@ RSpec.describe CatsController, type: :controller do
     }
   }
 
+  let(:update_attributes) {
+    {
+      name: "Mr. Fluffington",
+      picture: data_url
+    }
+  }
+
+  let(:invalid_attributes) {
+    {
+      name: "",
+      picture: data_url
+    }
+  }
+
   describe "GET index" do
     it "has a 200 status code" do
       get :index
@@ -36,6 +50,9 @@ RSpec.describe CatsController, type: :controller do
       get :index
       expect(response).to render_template("index")
     end
+  end
+
+  describe "Post #create cat " do
     it "creates a new cat" do
       expect { post :create, params: {cat: valid_attributes} }.to change(Cat, :count).by(1)
       expect(assigns(:cat).errors.size).to eq(0)
@@ -49,7 +66,7 @@ RSpec.describe CatsController, type: :controller do
       expect(result).to be true
     end
 
-    it " process the image to the propper mb size" do
+    it "process the image to the propper mb size" do
       post :create, params: {cat: valid_attributes}
       created_img = File.join(Rails.root,"public","#{assigns(:cat).picture}")
       expect(File.size(created_img)).to eq(80467)
@@ -64,7 +81,18 @@ RSpec.describe CatsController, type: :controller do
       #expect(FileUtils.identical?(created_img,comparison_image)).to be true
       expect(created_img).to be_identical_to(comparison_image)
     end
-
+    #     it "redirects to users#show" do
+    #       post :create, params: {user: valid_attributes}
+    #       expect(response).to redirect_to(User.last)
+    #     end
+    #     it "saves the new user avatar" do
+    #       post :create, params: {user: valid_attributes}
+    #       expect(assigns(:user).avatar_identifier).to eq("1.jpg")
+    #     end
+    #     it "produces valid create flash message" do
+    #       post :create, params: {user: valid_attributes}
+    #       expect(flash[:notice]).to match(I18n.t("user.successfully-created"))
+    #     end
   end
 
   describe "GET #show cat" do
@@ -82,47 +110,31 @@ RSpec.describe CatsController, type: :controller do
     end
   end
 
-  # describe "GET #new user" do
-  #   it "assigns a new User to @user" do
-  #     get :new
-  #     expect(assigns(:user)).to be_a_new(User)
-  #   end
-  #   it "renders the :new template" do
-  #     get :new
-  #     expect(response).to render_template("new")
-  #   end
-  # end
+  describe "GET #new cat" do
+    it "assigns a new User to @user" do
+      get :new
+      expect(assigns(:cat)).to be_a_new(Cat)
+    end
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
 
-  # describe "GET #edit user" do
-  #   it "assigns the requested user to @user" do
-  #     get :edit, params: {id: user_bob.to_param}
-  #     expect(assigns(:user)).to eq(user_bob)
-  #   end
-  #   it "renders the :edit template" do
-  #     get :edit, params: {id: user_bob.to_param}
-  #     expect(response).to render_template("edit")
-  #   end
-  # end
+  describe "GET #edit cat" do
+    before(:each) do
+      @cat = Cat.create(valid_attributes)
+    end
+    it "assigns the requested cat to @cat" do
+      get :edit, params: {id: @cat.to_param}
+      expect(assigns(:cat)).to eq(@cat)
+    end
+    it "renders the :edit template" do
+      get :edit, params: {id: @cat.to_param}
+      expect(response).to render_template("edit")
+    end
+  end
   # describe "POST #create user" do
-  #
-  #   context "with valid attributes" do
-  #     it "saves the new user in the database" do
-  #       expect { post :create, params: {user: valid_attributes} }.to change(User, :count).by(1)
-  #     end
-  #     it "redirects to users#show" do
-  #       post :create, params: {user: valid_attributes}
-  #       expect(response).to redirect_to(User.last)
-  #     end
-  #     it "saves the new user avatar" do
-  #       post :create, params: {user: valid_attributes}
-  #       expect(assigns(:user).avatar_identifier).to eq("1.jpg")
-  #     end
-  #     it "produces valid create flash message" do
-  #       post :create, params: {user: valid_attributes}
-  #       expect(flash[:notice]).to match(I18n.t("user.successfully-created"))
-  #     end
-  #   end
-  #
   #   context "with invalid attributes" do
   #     it "does not save the new user in the database" do
   #       expect { post :create, params: {user: invalid_attributes}}.to change(User, :count).by(0)
@@ -135,27 +147,35 @@ RSpec.describe CatsController, type: :controller do
 
   # end
 
-  # describe "PUT #update user" do
-  #
-  #   context "with valid attributes" do
-  #     it "updates the requested user" do
-  #       put :update, params: {id: user_bob.to_param, user: update_valid_attributes}
-  #       expect(assigns(:user)).to be_valid
-  #     end
-  #     it "updates the requested users image" do
-  #       put :update, params: {id: user_bob.to_param, user: update_valid_attributes}
-  #       expect(assigns(:user).avatar_identifier).to eq("2.jpg")
-  #     end
-  #     it "redirects to users#show" do
-  #       put :update, params: {id: user_bob.to_param, user: update_valid_attributes}
-  #       expect(response).to redirect_to(user_bob)
-  #     end
-  #     it "produces valid update flash message" do
-  #       put :update, params: {id: user_bob.to_param, user: update_valid_attributes}
-  #       expect(flash[:notice]).to match(I18n.t("user.successfully-updated"))
-  #     end
-  #
-  #   end
+
+
+
+
+  describe "PUT #update cat" do
+    before(:each) do
+      @cat = Cat.create(valid_attributes)
+    end
+    context "with valid attributes" do
+      it "updates the requested cat" do
+        put :update, params: {id: @cat.to_param, cat: update_attributes}
+        expect(assigns(:cat)).to be_valid
+      end
+      # it "updates the requested cats image" do
+      #   put :update, params: {id: user_bob.to_param, user: update_valid_attributes}
+      #   expect(assigns(:user).avatar_identifier).to eq("2.jpg")
+      # end
+      it "redirects to cats#show" do
+        put :update, params: {id: @cat.to_param, cat: update_attributes}
+        expect(response).to redirect_to(@cat)
+      end
+      it "produces valid update flash message" do
+        put :update, params: {id: @cat.to_param, cat: update_attributes}
+        expect(flash[:notice]).to match(I18n.t("cat.successfully-updated"))
+      end
+    end
+  end
+
+
 
   #   context "with invalid attributes" do
   #     it "assigns the user" do
