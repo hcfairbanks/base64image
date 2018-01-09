13b3701c34a74ed7588b6391f3c69b9f2d98eb23
Harry <hcfairbanks@gmail.com>
2018-01-08 22:26:07 -0500

    started converting uploader spec tests to minitest

---
+++ test/uploaders/picture_uploader_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+require 'minitest/autorun'
+
+# https://github.com/metaskills/minitest-spec-rails
+class MyJobTest < ActiveSupport::TestCase
+
+# http://guides.rubyonrails.org/testing.html
+# look up
+# 2.3 Rails meets Minitest
+
+  test "test of a test" do
+    x = PictureUploader.new()
+    assert_equal(5,5)
+  end
+
+
+end
